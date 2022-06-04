20057dd53c44729304b61c86a18ce35e253b538d
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-06-04 13:48:39 +0700

    operator

---
+++ bin/operator.dart
@@ -0,0 +1,19 @@
+class Orange {
+  int quantity = 0;
+
+  Orange operator +(Orange other) {
+    var result = Orange();
+    result.quantity = quantity + other.quantity;
+    return result;
+  }
+
+}
+
+void main(List<String> args) {
+  var orange1 = Orange();
+  orange1.quantity = 10;
+  var orange2 = Orange();
+  orange2.quantity = 20;
+  var orange3 = orange1 + orange2;
+  print(orange3.quantity);
+}
