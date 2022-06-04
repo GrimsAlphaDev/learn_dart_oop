022bdf8cf6782683dd069d0d12658c67656bb09e
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-06-04 23:56:22 +0700

    Error

---
+++ bin/error.dart
@@ -0,0 +1,11 @@
+/// Selain Exception, ada jenis kesalaha lagi yaitu error
+/// berbeda dengan Exception, Error adalahh jenis kesalahan yang harus dihindari, dan jika terjadi, lebih baik segera menghentingkan jalannya aplikasi
+/// error biasanya terjadi karena ada kesalahan pada kode program kita
+/// Contoh, kita mengakses index yang salah pada list
+
+
+void main(List<String> args) {
+  var list = [1, 2, 3, 4, 5];
+  // print(list[5]); Error karena tidak ada index 5
+  
+}
