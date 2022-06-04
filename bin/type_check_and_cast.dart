019935206878ce33552f5858e71479ba3d39fafd
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-06-04 18:38:19 +0700

    Type Check and Cast

---
+++ bin/type_check_and_cast.dart
@@ -0,0 +1,31 @@
+class Employee {
+  String name;
+  Employee(this.name);
+}
+
+class Manager extends Employee {
+  Manager(String name) : super(name);
+}
+
+class VicePresident extends Manager {
+  VicePresident(String name) : super(name);
+}
+
+void sayHello(Employee employee) {
+  if(employee is VicePresident ){
+    VicePresident vicePresident = employee as VicePresident;
+    print("Hello VP ${vicePresident.name}");
+  } else if(employee is Manager){
+    Manager manager = employee as Manager;
+    print("Hello Manager ${manager.name}");
+  } else {
+    print('Hello ${employee.name}');
+  }
+}
+
+void main(List<String> args) {
+  sayHello(Employee("John"));
+  sayHello(Manager("George"));
+  // sayHello("Paul"); Tidak Bisa karena pada method sayHello hanya bisa menerima turunan Employee 
+  sayHello(VicePresident("David"));
+}
