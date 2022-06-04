23ec989839c5ebd05ba4a9670ccf13839131c6b0
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-06-04 13:25:46 +0700

    Initialize Dart Project

---
+++ bin/learn_dart_oop.dart
@@ -0,0 +1,5 @@
+import 'package:learn_dart_oop/learn_dart_oop.dart' as learn_dart_oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_dart_oop.calculate()}!');
+}
---
+++ test/learn_dart_oop_test.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_oop/learn_dart_oop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
